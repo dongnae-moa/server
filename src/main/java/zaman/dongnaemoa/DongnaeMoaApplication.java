273c227168330b2b80d0fbbfd223ba6ad627725d
zaman <roblery128@gmail.com>
2026-08-04 15:54:42 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/zaman/dongnaemoa/DongnaeMoaApplication.java
@@ -0,0 +1,13 @@
+package zaman.dongnaemoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongnaeMoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongnaeMoaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zaman/dongnaemoa/DongnaeMoaApplicationTests.java
@@ -0,0 +1,13 @@
+package zaman.dongnaemoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongnaeMoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
